d58f59ebf629d1990c778ad56ede84c9590f37e7
Grant Ramsay <seapagan@gmail.com>
2022-07-14 14:16:05 +0100

    allow click to collapse current item

--- scripts/site.js
+++ scripts/site.js
@@ -6,10 +6,13 @@ const faqNodes = document.querySelectorAll(".faq-question");
 // handle the FAQ click
 const handleClick = (e) => {
   const parent = e.target.parentNode;
+  const current = document.querySelector(".active");
   faqNodes.forEach((element) => {
     element.parentNode.classList.remove("active");
   });
-  parent.classList.add("active");
+  if (parent !== current) {
+    parent.classList.add("active");
+  }
 };
 // add click handlers to all FAQ elements
 faqNodes.forEach((element) => {
